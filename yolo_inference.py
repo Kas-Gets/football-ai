33c7e5a3e51288d38abe6bac8f0c31d866ac4365
Kas-Gets <kasget1@gmail.com>
2026-07-10 19:23:27 +0300

    load a  YOLO26x model that i trained on google colab

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
-model = YOLO('yolo26x.pt')  # load a pretrained YOLO26x model
+model = YOLO('models/best.pt')  # load a  YOLO26x model that i trained on google colab
 
 results = model.predict(source ='input_videos/08fd33_4.mp4',show =False, conf=0.5, save=True, project='/home/kas/football/outputs', name='test', exist_ok=True)
 print(results[0])
