704483af5788f4636bbfc6b054395312ef1e9151
Kas-Gets <kasget1@gmail.com>
2026-07-11 14:15:38 +0300

    implementing tracker and prediction

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from utils.video_utils import read_video, save_video
+from trackers import Tracker
 
 def main():
     # Read video frames from the input video
     video_frames = read_video('input_videos/08fd33_4.mp4') 
+
+    # Initialize the Tracker
+    tracker = Tracker(model_path='models/best.pt')
+
+    tracks = tracker.get_object_tracks(video_frames,
+                                         read_from_stub=True, 
+                                         stub_path='stubs/track_stubs.pkl')
 
     # Save the video frames to an output video file    
     save_video(video_frames, 'output_videos/output.mp4')
