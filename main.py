f46fec03bc21bf473cf0f7fb9aefeb4d4db956c7
Kas-Gets <kasget1@gmail.com>
2026-07-15 17:44:06 +0300

    putting into module for the team assigner cluster(jersey color)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils.video_utils import read_video, save_video
 from trackers import Tracker
 import cv2
+from team_assigner import TeamAssigner
 
 def main():
     # Read video frames from the input video
@@ -12,6 +13,7 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                          read_from_stub=True, 
                                          stub_path='stubs/track_stubs.pkl')
+    
 
     #save cropped images of players
     for track_id, player in tracks['players'][0].items():
@@ -23,6 +25,19 @@ def main():
 
         #save cropped player image
         cv2.imwrite(f'output_images/player_{track_id}.jpg', cropped_player)
+
+    # Assign teams to players
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], tracks['players'][0])
+
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            bbox = track['bbox']
+            team = team_assigner.get_player_team(video_frames[frame_num],
+                                                track['bbox'],
+                                                player_id)
+            tracks['players'][frame_num][player_id]['team'] = team
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
     #Draw output
     ##Draw object Tracks
